bd32e20a5b11d7e8bfb771eb496297d1c4472323
Walter Chen <wachen@mozilla.com>
2016-10-21 15:17:08 +0800

    Create bootstrap.bat

---
+++ bootstrap.bat
@@ -0,0 +1,75 @@
+REM Author: Walter Chen
+REM Version: 0.0.1
+
+REM Assuming that you already git pull all the files, we can install curl from the repository
+msiexec /i curl-7.46.0-win32-local.msi /quiet /qn /norestart /log curl.log
+
+
+::::::::::::::::::::
+::  Prerequisite  ::
+::::::::::::::::::::
+
+REM Checking Java
+
+REM Checking Curl
+
+REM Checking Git
+
+
+::::::::::::::::::::
+::  Installation  ::
+::::::::::::::::::::
+
+REM Installing 7zip
+
+REM Installing Miniconda
+
+REM Installing winpy32
+
+REM Installing opencv2
+
+REM Installing mitmproxy
+
+REM Installing ffmpeg
+
+REM Installing Sikuli
+
+
+::::::::::::::::::::
+::  Hasal  Setup  ::
+::::::::::::::::::::
+
+REM Creating virtualenv from conda
+
+REM Installing numpy and scipy
+
+REM Installing Hasal
+
+
+::::::::::::::::::::
+::    Checking    ::
+::::::::::::::::::::
+
+REM Checking CV2
+
+REM Checking system packages
+
+
+::::::::::::::::::::
+::    Browsers    ::
+::::::::::::::::::::
+
+REM Installing chrome
+
+REM Installing firefox
+
+
+::::::::::::::::::::
+::    Finished    ::
+::::::::::::::::::::
+
+REM Bootstrap done
+
+
+
+
