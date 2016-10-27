0bc541911b4660ff53e5ca78281e2491e120fe00
Walter Chen <wachen@mozilla.com>
2016-10-27 18:25:09 +0800

    SkipCI

--- bootstrap.bat
+++ bootstrap.bat
@@ -155,6 +155,10 @@ IF "%APPVEYOR%"=="True" (
     activate hasal-env & pip install mitmproxy thirdParty\opencv_python-2.4.13-cp27-cp27m-win32.whl & certutil -p "" thirdParty\mitmproxy-ca-cert.p12 & python setup.py install & python scripts\cv2_checker.py
 )
 
+:::::::::::::::::::::::::::::::::::::::::::::::::::To be taken out.
+cd ..
+:::::::::::::::::::::::::::::::::::::::::::::::::::
+
 ::::::::::::::::::::
 ::    Finished    ::
 ::::::::::::::::::::
